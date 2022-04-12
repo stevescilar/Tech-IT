f4749033633210c72c78c401c84c47b2f68c7978
stevescilar <stevemuambi@gmail.com>
2022-04-12 16:32:24 +0300

    updated

--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     stock         = models.IntegerField()
     is_available  = models.BooleanField(default=True)
     category      = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_date  = models.DateTimeField(auto_add_now=True)
+    created_date  = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
 
