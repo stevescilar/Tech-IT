b1e04b71786bf0036ab4533dd88958b9a0a08992
stevescilar <stevemuambi@gmail.com>
2022-04-12 12:05:48 +0300

    updated

--- accounts/models.py
+++ accounts/models.py
@@ -13,12 +13,30 @@ class MyAccountManager(BaseUserManager):
             raise ValueError('User must have a username')
 
         user = self.model(
-            email = self.normalize_email(email)
+            email = self.normalize_email(email),
             username = username,
             first_name = first_name,
             last_name = last_name,        
         )
 
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,first_name,last_name,username, email,password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active =  True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
         
 class Account(AbstractBaseUser):
     first_name      = models.CharField(max_length=50)
@@ -37,6 +55,8 @@ class Account(AbstractBaseUser):
 
     USERNAME_FIELD  = 'email'
     REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    objects = MyAccountManager()
 
     def __str__(self):
         return self.email
--- techit/settings.py
+++ techit/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'techit.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
