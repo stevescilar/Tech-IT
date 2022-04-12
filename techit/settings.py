c34ec89a257be7b3a308c3b651637401f4cee28a
stevescilar <stevemuambi@gmail.com>
2022-04-12 15:39:35 +0300

    updated

--- techit/settings.py
+++ techit/settings.py
@@ -127,6 +127,11 @@ STATICFILES_DIRS = [
     'techit/static',
 ]
 
+# midea files configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
