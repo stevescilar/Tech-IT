45c4d6e03c8a210add2cca3dc1e07da43b8e2434
stevescilar <stevemuambi@gmail.com>
2022-04-12 18:55:46 +0300

    updated

--- store/admin.py
+++ store/admin.py
@@ -7,7 +7,7 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields = {
-        'slug':(('product_name',))
+        'slug':('product_name',)
         }
 
 admin.site.register(Product, ProductAdmin)
