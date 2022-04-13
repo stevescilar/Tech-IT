e9f1798630a2c05fb5fc7c98019069d881ba8461
stevescilar <stevemuambi@gmail.com>
2022-04-13 23:28:39 +0300

    updated

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.store, name='store')
+    path('',views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
 ]
