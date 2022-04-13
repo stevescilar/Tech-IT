83beb5603810f9fb971bb31ae9699c0321267556
stevescilar <stevemuambi@gmail.com>
2022-04-13 10:09:25 +0300

    updated

--- store/views.py
+++ store/views.py
@@ -5,8 +5,10 @@ from store.models import Product
 
 def store(request):
     products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
 
     context = {
-        'products':products
+        'products': products,
+        'product_count': product_count
     }
     return render (request, 'store/store.html',context)
