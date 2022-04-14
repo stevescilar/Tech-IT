525b8650dc9e55ed83d6280d63f683bdf84916cc
stevescilar <stevemuambi@gmail.com>
2022-04-14 19:20:08 +0300

    updated

--- store/views.py
+++ store/views.py
@@ -28,6 +28,7 @@ def product_detail(request, category_slug, product_slug):
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
     except Exception as e:
         raise e 
+        
     context  = {
         'single_product':single_product,
     }
