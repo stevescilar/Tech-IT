18449af358b0e449e4ddc78cdb67e48ed66d854e
stevescilar <stevemuambi@gmail.com>
2022-04-15 09:55:55 +0300

    updated

--- carts/admin.py
+++ carts/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Cart, CartItem
 # Register your models here.
 
-admin.site.register(Cart, CartItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
 
--- carts/models.py
+++ carts/models.py
@@ -1,4 +1,3 @@
-from email.policy import default
 from django.db import models
 from store.models import Product
 
