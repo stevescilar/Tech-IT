4a238def92dda1f41a49d8ad9547eeaf7142114d
stevescilar <stevemuambi@gmail.com>
2022-04-20 13:28:27 +0300

    updated

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import Cart, CartItem
 from store.models import Product
 
@@ -35,6 +35,18 @@ def add_cart(request, product_id):
         cart_item.save()
 
     return redirect('cart')
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart = cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+    
 
 
 
