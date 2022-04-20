682c768842607517c76bb823ceb6197258d119bb
stevescilar <stevemuambi@gmail.com>
2022-04-20 13:34:31 +0300

    updated

--- carts/views.py
+++ carts/views.py
@@ -46,8 +46,13 @@ def remove_cart(request, product_id):
     else:
         cart_item.delete()
     return redirect('cart')
-    
 
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart = cart)
+    cart_item.delete()
+    return redirect('cart')
 
 
 
