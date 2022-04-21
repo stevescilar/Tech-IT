b86a6099d2af12133fb600f0642f646635676ce6
stevescilar <stevemuambi@gmail.com>
2022-04-21 13:47:22 +0300

    register_update

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from . models import Account
 
 class RegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
     class Meta:
         model = Account
         fields = [
             'first_name','last_name','email','phone_number','password'
         ]
 
-        
