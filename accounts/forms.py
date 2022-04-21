aab4f29bb19bbc679af779cf1fa2d2c065195d23
stevescilar <stevemuambi@gmail.com>
2022-04-21 13:35:39 +0300

    accounts buildup

---
+++ accounts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from . models import Account
+
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = [
+            'first_name','last_name','email','phone_number','password'
+        ]
+
+        
