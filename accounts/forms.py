cf3c226dee2eb4dcec3af2015256d60088f4213b
stevescilar <stevemuambi@gmail.com>
2022-04-21 15:36:07 +0300

    registration complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,9 +22,16 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Your Email Address'
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Your Phone Number'
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter Your First Name'
-        
-
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password') 
+
+        if password !=confirm_password:
+            raise forms.ValidationError(
+                "password & confirm password  does not match!"
+            )
 
--- techit/settings.py
+++ techit/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,8 @@ MEDIA_ROOT = BASE_DIR /'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
