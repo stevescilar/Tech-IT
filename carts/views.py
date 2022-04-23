5ec9c7eb701085e43c951f5e6c381b1747ab8097
stevescilar <stevemuambi@gmail.com>
2022-04-23 12:31:22 +0300

    cart bug

--- carts/views.py
+++ carts/views.py
@@ -136,10 +136,14 @@ def add_cart(request, product_id):
         return redirect('cart')
 
 def remove_cart(request, product_id, cart_item_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
+    
     product = get_object_or_404(Product,id=product_id)
     try:
-        cart_item = CartItem.objects.get(product=product, cart = cart, id=cart_item_id )
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id )
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_item = CartItem.objects.get(product=product, cart = cart, id=cart_item_id )
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
@@ -149,10 +153,13 @@ def remove_cart(request, product_id, cart_item_id):
         pass
     return redirect('cart')
 
-def remove_cart_item(request,product_id,cart_item_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
+def remove_cart_item(request,product_id,cart_item_id): 
     product = get_object_or_404(Product,id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart = cart, id=cart_item_id)
+    if request.user.is_authenticated:
+        cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id )
+    else:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(product=product, cart = cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
