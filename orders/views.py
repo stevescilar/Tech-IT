20ab4f0fbd29e7575c55e35bd549d9321db585e8
stevescilar <stevemuambi@gmail.com>
2022-04-26 13:12:58 +0300

    paypal-complete

--- orders/views.py
+++ orders/views.py
@@ -6,10 +6,27 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order
-
+from .models import Order, Payment
+import json
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number = body['orderID'])
+    # store details on payment models
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+
+    )
+    payment.save()
+
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
     return render(request,'orders/payments.html')
 
 
