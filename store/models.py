730fab752fc224e5717ea413b3ff272f36184add
stevescilar <stevemuambi@gmail.com>
2022-04-27 16:12:27 +0300

    like a star

--- store/models.py
+++ store/models.py
@@ -1,7 +1,9 @@
 from tkinter import CASCADE
 from django.db import models
+from django.forms import CharField, FloatField
 from  category.models import Category
 from django.urls import reverse
+from accounts.models import Account
 
 # Create your models here.
 class Product(models.Model):
@@ -48,4 +50,20 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20,blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
 
