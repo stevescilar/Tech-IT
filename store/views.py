ca9e616950524a14aac40e4b9b7afef4695d8538
stevescilar <stevemuambi@gmail.com>
2022-04-28 12:50:52 +0300

    updated_ratings

--- store/views.py
+++ store/views.py
@@ -43,12 +43,14 @@ def product_detail(request, category_slug, product_slug):
 
     except Exception as e:
         raise e 
-
-    try:
-        orderproduct = OrderProduct.objects.filter(user=request.user, product_id = single_product.id).exists()
-    except OrderProduct.DoesNotExist:
-        orderproduct = None
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id = single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
         
+    else:
+        orderproduct = None
 
     reviews = ReviewRating.objects.filter(product_id=single_product.id,status = True)
 
