9b1620331c227b430a25b9433710006d2f353d53
stevescilar <stevemuambi@gmail.com>
2022-04-28 15:37:14 +0300

    dashboard=user

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from .models import Account
 from django.contrib import messages , auth
 from carts.views import _cart_id
 from carts.models import Cart,CartItem  
+from orders.models import Order
  
 
 # verification email
@@ -157,7 +158,12 @@ def activate(request,uidb64, token):
 
 @login_required(login_url = 'login')
 def dashboard(request):
-    return render (request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered = True)
+    orders_count = orders.count()
+    context = {
+        'orders_count':orders_count,
+    }
+    return render (request, 'accounts/dashboard.html',context)
 
 def forgotPassword(request):
     if request.method =='POST':
@@ -220,3 +226,11 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request,'accounts/resetPassword.html')
+
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user,is_ordered=True).order_by('-created_at')
+    context = {
+        'orders':orders,
+    }
+    return render (request,'accounts/my_orders.html',context)
