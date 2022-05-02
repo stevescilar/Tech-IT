44b88d83c7839c8da7ba54994020085016678c8c
stevescilar <stevemuambi@gmail.com>
2022-05-02 14:20:00 +0300

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from .models import Account, UserProfile
 from django.contrib import messages , auth
 from carts.views import _cart_id
 from carts.models import Cart,CartItem  
-from orders.models import Order
+from orders.models import Order, OrderProduct
  
 
 # verification email
@@ -285,3 +285,17 @@ def change_password(request):
             messages.error(request,'Password does not match')
             return redirect ('change_password')
     return render(request,'accounts/change_password.html')
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number = order_id)
+    order = Order.objects.get(order_number = order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail' : order_detail,
+        'order'         : order,
+        'subtotal'      : subtotal,
+    }
+    return render(request,'accounts/order_detail.html',context)
