de5507fdddcaa076d08ad4ea65008599dfe77e0d
stevescilar <stevemuambi@gmail.com>
2022-05-13 16:21:20 +0300

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -159,10 +159,11 @@ def activate(request,uidb64, token):
 
 @login_required(login_url = 'login')
 def dashboard(request):
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
     # userprofile = get_object_or_404(UserProfile, user=request.user)
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered = True)
     orders_count = orders.count()
-    userprofile = UserProfile.objects.get(user_id=request.user.id)
+   
     context = {
         'orders_count':orders_count,
         'userprofile' : userprofile,
--- techit/views.py
+++ techit/views.py
@@ -5,11 +5,12 @@ from store.models import Product,ReviewRating
 
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('-created_date')
+    reviews = ''
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id,status = True)
 
     context = {
         'products':products,
-        'reviews'   :reviews,
+        'reviews' :reviews,
     }
     return render(request, 'home.html',context)
